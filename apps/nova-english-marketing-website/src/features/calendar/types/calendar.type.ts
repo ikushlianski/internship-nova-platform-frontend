fba4109c02733c93e7c2ba2bf225324078b8b566
Ilya Kushlianski <kushliansky@gmail.com>
2024-08-21 11:55:58 +0300

    Folder structure suggestion: feature-centric approach
    Signed-off-by: Ilya Kushlianski <kushliansky@gmail.com>

---
+++ apps/nova-english-marketing-website/src/features/calendar/types/calendar.type.ts
@@ -0,0 +1,10 @@
+export type Calendar = {
+  id: string;
+  name: string;
+  description: string;
+  location: string;
+  startDate: string;
+  endDate: string;
+  allDay: boolean;
+  color: string;
+}
---
+++ apps/nova-english-marketing-website/src/features/calendar/types/event.type.ts
@@ -0,0 +1,7 @@
+export type Event = {
+  id: string;
+  title: string;
+  description: string;
+  endDate: string;
+  allDay: boolean;
+};

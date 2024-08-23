0ffc2a9c006ef964bcd9bfb06f51a55f89115e88
Svygzhryr <pamixy@gmail.com>
2024-08-23 11:26:07 +0300

    feat: finish basic cards, decks, settings mocks

--- apps/spaced-rep-tool-web/src/App.tsx
+++ apps/spaced-rep-tool-web/src/App.tsx
@@ -1,20 +1,6 @@
-import { useEffect } from "react";
-import { url } from "./mocks/handlers";
 import "./App.css";
 
 function App() {
-  async function getCards() {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  }
-
-  useEffect(() => {
-    getCards().then((response) => {
-      console.log(response);
-    });
-  }, []);
-
   return <></>;
 }
 

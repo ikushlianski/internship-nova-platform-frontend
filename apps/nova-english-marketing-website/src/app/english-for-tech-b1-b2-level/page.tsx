5e99f2de6967108be5ad73a8ac1de168edb37877
Anastasiya Andronova <anastasiya5.88@mail.ru>
2024-08-25 23:32:00 +0300

    feat: implement button component

--- apps/nova-english-marketing-website/src/app/english-for-tech-b1-b2-level/page.tsx
+++ apps/nova-english-marketing-website/src/app/english-for-tech-b1-b2-level/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Button } from '@repo/ui/button';
 
 export default function Page() {
   return (
     <div>
       <h1>Hello im B1 and B2 level course!!</h1>
+      <Button className="bg-blue-500 text-white-foreground" appName='WBS' imageLink={'assets/icons/button_present.svg'} form='REC' size="L">BUTTON</Button>
+      <Button className="bg-disabled text-foreground" appName='WBS' imageLink={'assets/icons/button_present.svg'} form='REC' size="S">BUTTON</Button>
+      <Button className="bg-blue-50" appName='WBS' imageLink={'assets/icons/arrow_blue.svg'} form='ROUND' size="L" outline='TRUE'></Button>
+      <Button className="bg-disabled text-foreground" appName='WBS' imageLink={'assets/icons/arrow_white.svg'} form='ROUND' size="S"></Button>
     </div>
   )
 }
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,20 +1,40 @@
-"use client";
+'use client';
 
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
+import styles from './button.module.css';
 
 interface ButtonProps {
-  children: ReactNode;
+  children?: ReactNode;
   className?: string;
   appName: string;
+  form: 'REC' | 'ROUND';
+  size: 'L' | 'S';
+  imageLink?: string;
+  outline?: string;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({
+  children,
+  className,
+  appName,
+  form = 'REC',
+  size = 'L',
+  imageLink,
+  outline,
+}: ButtonProps) => {
   return (
     <button
-      className={className}
+      className={`${className || ''} ${styles.button} ${form === 'REC' ? size === 'L' ? styles.large :styles.small : styles.round} ${outline ? styles.outline : ''}`}
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
       {children}
+      {imageLink && (
+        <img
+          src={imageLink}
+          alt="Button Icon"
+          className={`${styles.image} ${form === 'REC' ? size === 'L' ? styles.recl : styles.recs : size === 'L' ? styles.roundl : styles.rounds } ${className || ''}`}
+        />
+      )}
     </button>
   );
 };

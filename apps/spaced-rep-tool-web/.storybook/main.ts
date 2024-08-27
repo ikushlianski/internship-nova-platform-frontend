bcaa1df7d981623d03c6844665eba30aa91cd5c7
Wadimx199789 <martinkewichwadim@gmail.com>
2024-08-28 00:43:31 +0300

    SP-68: added tailwind

--- apps/spaced-rep-tool-web/.storybook/main.ts
+++ apps/spaced-rep-tool-web/.storybook/main.ts
@@ -1,10 +1,7 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: [
-    "../src/components/**/*.mdx",
-    "../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
@@ -12,10 +9,14 @@ const config: StorybookConfig = {
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
   ],
+
   framework: {
-    name: "@storybook/nextjs",
+    name: "@storybook/react-vite",
     options: {},
   },
-  //   staticDirs: ["..\\public"],
+
+  docs: {
+    autodocs: true,
+  },
 };
 export default config;
--- apps/spaced-rep-tool-web/src/main.tsx
+++ apps/spaced-rep-tool-web/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "./globals.css";
 
 async function enableMocking() {
   if (process.env.API_MOCKING !== "true") {

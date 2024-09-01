b1669708f19ee73bc2175ca208cd68166477d8ed
Anastasiya Andronova <anastasiya5.88@mail.ru>
2024-09-01 22:56:46 +0300

    fix: delete test components from english for tech page

--- apps/nova-english-marketing-website/src/app/english-for-tech-b1-b2-level/page.tsx
+++ apps/nova-english-marketing-website/src/app/english-for-tech-b1-b2-level/page.tsx
@@ -1,106 +1,10 @@
-'use client';
-
 import React from 'react';
-import { Button } from '@repo/ui/button';
-import { InputComponent } from '@repo/ui/input';
-import { SelectComponent } from '@repo/ui/select';
-import { LinkComponent } from '../components/link';
 
 
 export default function Page() {
-  const countries = [
-    {
-      name: 'Belarus',
-      content: '<img src="assets/icons/countryBelarus.svg"/>',
-    },
-    {
-      name: 'Italy',
-      content: '<img src="assets/icons/arrow_blue.svg"/>',
-    },
-    {
-      name: 'Greece',
-      content: '<img src="assets/icons/button_present.svg"/>',
-    },
-  ];
-  const callback = () => {
-    console.log('Hello');
-  };
-
   return (
     <div>
       <h1>Hello im B1 and B2 level course!!</h1>
-      <Button
-        classNameButton="bg-blue-500 text-white-foreground"
-        classNameImage="bg"
-        appName="WBS"
-        imageLink={'assets/icons/button_present.svg'}
-        callback={callback}
-      >
-        Button
-      </Button>
-      <Button
-        classNameButton="bg-disabled text-foreground "
-        classNameImage="bg"
-        appName="WBS"
-        imageLink={'assets/icons/button_present.svg'}
-        callback={callback}
-      >
-        BUTTON
-      </Button>
-      <Button
-        classNameButton="bg-blue-50"
-        classNameImage="bg"
-        appName="WBS"
-        imageLink={'assets/icons/arrow_blue.svg'}
-        callback={callback}
-      ></Button>
-      <Button
-        classNameButton="bg-disabled text-foreground"
-        appName="WBS"
-        imageLink={'assets/icons/arrow_white.svg'}
-        callback={callback}
-      ></Button>
-      <InputComponent
-        classNameComponent="bg"
-        classNameInput="bg"
-        classNameSelect="bg"
-        appName="WBS"
-        placeholder="Text"
-        type="input"
-      />
-      <InputComponent
-        classNameComponent="bg"
-        classNameInput="bg"
-        classNameSelect="bg"
-        appName="WBS"
-        placeholder="Text"
-        type="input"
-        props={countries}
-      />
-      <InputComponent
-        classNameComponent="bg"
-        classNameInput="bg"
-        classNameSelect="bg"
-        appName="WBS"
-        placeholder="Text"
-        type="input"
-        props={countries}
-      />
-      <SelectComponent
-        classNameSelect="bg"
-        appName="WBS"
-        type="select"
-        props={countries}
-      />
-      <SelectComponent
-        classNameSelect="bg"
-        appName="WBS"
-        type="select"
-        props={countries}
-      />
-      <LinkComponent appName="WBS" navLink={[{ title: 'Home', path: '/' }]}>
-        Link
-      </LinkComponent>
     </div>
   );
 }

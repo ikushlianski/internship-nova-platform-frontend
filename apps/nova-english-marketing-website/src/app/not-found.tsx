362e5e08c7c66a694793d01f8dff66a45eca662f
Yury Kinchin <yury.kinchyn@gmail.com>
2024-09-03 17:26:36 +0300

    MWS-72: implement 404 page

--- apps/nova-english-marketing-website/src/app/not-found.tsx
+++ apps/nova-english-marketing-website/src/app/not-found.tsx
@@ -2,7 +2,11 @@ import NotFound from "@/features/NotFound/ui/NotFound.component"
 
 const NotFoundPage = () => {
 	return (
-		<div>
+		<div className="pt-16 flex flex-col justify-center items-center gap-16">
+			<h1 className="text-xl font-bold">
+				К сожалению, страница, которую вы ищете, не существует или была
+				перемещена
+			</h1>
 			<NotFound />
 		</div>
 	)

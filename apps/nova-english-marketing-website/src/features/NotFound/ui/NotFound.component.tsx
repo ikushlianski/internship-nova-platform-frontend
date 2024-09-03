e7e2e14b31c38182f96f1867a8e1dd999e691724
Yury Kinchin <yury.kinchyn@gmail.com>
2024-09-03 16:36:18 +0300

    MWS-39: rename entity Course to Class

--- apps/nova-english-marketing-website/src/features/NotFound/ui/NotFound.component.tsx
+++ apps/nova-english-marketing-website/src/features/NotFound/ui/NotFound.component.tsx
@@ -1,24 +1,19 @@
 import TGLogo from "@/assets/icons/telegramLogo.svg"
 import WALogo from "@/assets/icons/whatsAppLogo.svg"
-import {
-	Card,
-	CardDescription,
-	CardHeader,
-	CardTitle
-} from "@/features/Card/Card.component"
-import { fetchCourses } from "@/features/Carousel/api/fetchMockCourses"
+import { Class } from "@repo/shared-types/class"
+import { Card, CardDescription, CardHeader, CardTitle } from "@repo/ui/card"
 import {
 	Carousel,
 	CarouselContent,
 	CarouselItem,
 	CarouselNext,
 	CarouselPrevious
-} from "@/features/Carousel/ui/Carousel.component"
-import { Course } from "@repo/shared-types/course"
+} from "@repo/ui/carousel"
 import Link from "next/link"
+import { fetchCourses } from "../api/fetchMockCourses"
 
 const NotFound = async () => {
-	let courses: Course[] = []
+	let courses: Class[] = []
 
 	let error: string | null = null
 
@@ -41,17 +36,17 @@ const NotFound = async () => {
 			<h1 className="text-2xl">Доступные курсы:</h1>
 			<Carousel className="w-2/3">
 				<CarouselContent>
-					{courses.map(({ courseId, courseName, courseLevel, courseTime }) => (
+					{courses.map(({ classId, className, classLevel, classTime }) => (
 						<CarouselItem
-							key={courseId}
+							key={classId}
 							className="md:basis-1/2 lg:basis-1/3"
 						>
-							<Link href={`/course/${courseId}`}>
+							<Link href={`/course/${classId}`}>
 								<Card className="border-2 cursor-pointer">
 									<CardHeader>
-										<CardTitle>{courseName}</CardTitle>
-										<CardDescription>{courseLevel}</CardDescription>
-										<CardDescription>{courseTime}</CardDescription>
+										<CardTitle>{className}</CardTitle>
+										<CardDescription>{classLevel}</CardDescription>
+										<CardDescription>{classTime}</CardDescription>
 									</CardHeader>
 								</Card>
 							</Link>

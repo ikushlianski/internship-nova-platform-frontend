aab577b64271d4d58a07e381acffc30cd259cad3
Louisa Sapina <luiza@MacBook-Air-Sapina.local>
2024-09-03 22:49:47 +0500

    fix: saving changes to App.tsx and RoutesEnum.ts files

--- apps/spaced-rep-tool-web/src/App.tsx
+++ apps/spaced-rep-tool-web/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { PrivacyPolicy } from "./app/PrivacyPolicy/PrivacyPolicy";
 import { RoutesEnum } from "./utils/RoutesEnum";
 import { Main } from "./app/Main/Main";
+import SignIn from "./features/SignIn/SignIn";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Route path={RoutesEnum.Main} element={<Main />} />
         <Route index element={<Navigate to={RoutesEnum.PrivacyPolicy} />} />
         <Route path={RoutesEnum.PrivacyPolicy} element={<PrivacyPolicy />} />
+        <Route path={RoutesEnum.Login} element={<SignIn />} />
+
       </Routes>
     </div>
   );

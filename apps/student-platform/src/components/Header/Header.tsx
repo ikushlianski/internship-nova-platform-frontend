ac25aaa31eeba64508813eb2ff569835c5c0b3e7
Nikita8Sannikov <Ak-4t7@mail.ru>
2024-09-04 20:13:06 +0600

    fixed emty alts in Header component

--- apps/student-platform/src/components/Header/Header.tsx
+++ apps/student-platform/src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ export const Header = () => {
         <div className="w-16 h-16 overflow-hidden">
             <img src="https://cdn-icons-png.flaticon.com/256/5309/5309804.png"
                 className="w-full h-full object-cover"
-            alt=""></img>
+            alt="english book"></img>
         </div>
         <div className="flex gap-5 items-center">
             <div className="w-8 h-8 bg-gray-300"></div>
@@ -12,7 +12,7 @@ export const Header = () => {
                 <div className="w-16 h-16 rounded-full overflow-hidden">
                     <img src="https://cdn-icons-png.flaticon.com/256/3135/3135715.png"
                         className="w-full h-full object-cover"
-                   alt="" ></img>
+                   alt="person" ></img>
                 </div>
                 <p>Someone</p>
             </div>

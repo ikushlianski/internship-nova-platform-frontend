da13af259d7005db83477af436dc7d09c53467d5
Yury Kinchin <yury.kinchyn@gmail.com>
2024-09-07 18:11:34 +0300

    MWS-32: add RootLayout

---
+++ apps/nova-english-marketing-website/src/app/layout.tsx
@@ -0,0 +1,41 @@
+import { locales } from "@/i18n/config"
+import { getTranslations, unstable_setRequestLocale } from "next-intl/server"
+import { Manrope } from "next/font/google"
+import { ReactNode } from "react"
+import "./globals.css"
+
+type Props = {
+	children: ReactNode
+	params: { locale: string }
+}
+
+const manrope = Manrope({
+	subsets: ["latin", "cyrillic"],
+	variable: "--font-manrope",
+	weight: ["200", "300", "400", "500", "600", "700", "800"]
+})
+
+export function generateStaticParams() {
+	return locales.map((locale) => ({ locale }))
+}
+
+export async function generateMetadata({
+	params: { locale }
+}: Omit<Props, "children">) {
+	const t = await getTranslations({ locale, namespace: "locale_layout" })
+
+	return {
+		title: t("title"),
+		description: t("description")
+	}
+}
+
+export default function RootLayout({ children, params: { locale } }: Props) {
+	unstable_setRequestLocale(locale)
+
+	return (
+		<html lang={locale}>
+			<body className={manrope.className}>{children}</body>
+		</html>
+	)
+}

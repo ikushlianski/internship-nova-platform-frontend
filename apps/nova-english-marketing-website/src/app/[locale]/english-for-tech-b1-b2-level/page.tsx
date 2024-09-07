0acdfc2b456cc4b5d361bcd78c34f0f221f9b249
Yury Kinchin <yury.kinchyn@gmail.com>
2024-09-07 18:11:17 +0300

    MWS-32: move markup to upper component

--- apps/nova-english-marketing-website/src/app/[locale]/english-for-tech-b1-b2-level/page.tsx
+++ apps/nova-english-marketing-website/src/app/[locale]/english-for-tech-b1-b2-level/page.tsx
@@ -3,49 +3,8 @@ import { useTranslations } from "next-intl"
 export default function Page() {
 	const t = useTranslations("temporary")
 	return (
-		<main>
-			<div>
-				<h1>{t("b1_b2_level_course")}</h1>
-			</div>
-			<section>
-				<h2>{t("hero_section")}</h2>
-			</section>
-			<section>
-				<h2>{t("path_section")}</h2>
-			</section>
-			<section>
-				<h2>{t("course_section")}</h2>
-			</section>
-			<section>
-				<h2>{t("teachers_section")}</h2>
-			</section>
-			<section>
-				<h2>{t("who_us_section")}</h2>
-			</section>
-			<section>
-				<h2>{t("trial_lesson_section")}</h2>
-			</section>
-			<section>
-				<h2>{t("program_of_course_section")}</h2>
-			</section>
-			<section>
-				<h2>{t("price_section")}</h2>
-			</section>
-			<section>
-				<h2>{t("test_section")}</h2>
-			</section>
-			<section>
-				<h2>{t("contact_form_section")}</h2>
-			</section>
-			<section>
-				<h2>{t("faq_section")}</h2>
-			</section>
-			<section>
-				<h2>{t("support_common_section")}</h2>
-			</section>
-			<section>
-				<h2>{t("common_section")}</h2>
-			</section>
-		</main>
+		<div>
+			<h1>{t("b1_b2_level_course")}</h1>
+		</div>
 	)
 }

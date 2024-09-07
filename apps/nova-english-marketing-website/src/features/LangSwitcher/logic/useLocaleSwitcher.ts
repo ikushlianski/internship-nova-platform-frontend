50a131540395ad96f4a6da444ae511c4b944aee5
Yury Kinchin <yury.kinchyn@gmail.com>
2024-09-07 18:15:53 +0300

    MWS-32: add locale switcher hook

---
+++ apps/nova-english-marketing-website/src/features/LangSwitcher/logic/useLocaleSwitcher.ts
@@ -0,0 +1,25 @@
+import { usePathname, useRouter } from "@/i18n/navigation"
+import { useParams } from "next/navigation"
+import { ChangeEvent, useTransition } from "react"
+
+export function useLocaleSwitcher() {
+	const router = useRouter()
+	const [isPending, startTransition] = useTransition()
+	const pathname = usePathname()
+	const params = useParams()
+	const handleLocaleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+		const nextLocale = event.target.value
+		startTransition(() => {
+			router.push(
+				// @ts-ignore
+				{ pathname, params },
+				{ locale: nextLocale }
+			)
+		})
+	}
+
+	return {
+		handleLocaleChange,
+		isPending
+	}
+}

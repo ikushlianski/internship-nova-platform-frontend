1a106243496a1cb48a97ec300edf36becb968a9e
Yury Kinchin <yury.kinchyn@gmail.com>
2024-09-07 18:12:50 +0300

    MWS-32: replace hardcoded text with internationalization

--- apps/nova-english-marketing-website/src/app/not-found.tsx
+++ apps/nova-english-marketing-website/src/app/not-found.tsx
@@ -1,12 +1,12 @@
 import NotFound from "@/features/NotFound/ui/NotFound.component"
+import { useTranslations } from "next-intl"
 
 const NotFoundPage = () => {
+	const t = useTranslations("not_found")
+
 	return (
 		<div className="pt-16 flex flex-col justify-center items-center gap-16">
-			<h1 className="text-xl font-bold">
-				К сожалению, страница, которую вы ищете, не существует или была
-				перемещена
-			</h1>
+			<h1 className="text-xl font-bold">{t("title")}</h1>
 			<NotFound />
 		</div>
 	)

5db0dd01013a775c645ee5f955428fe8f6d3bf7b
Ilya Kushlianski <kushliansky@gmail.com>
2024-09-08 10:07:50 +0300

    MEMO-7: Fix sign-in sign-up flow
    Signed-off-by: Ilya Kushlianski <kushliansky@gmail.com>

--- apps/spaced-rep-tool-web/src/App.tsx
+++ apps/spaced-rep-tool-web/src/App.tsx
@@ -1,19 +1,17 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import "./App.css";
-import { PrivacyPolicy } from "./app/PrivacyPolicy/PrivacyPolicy";
-import { RoutesEnum } from "./utils/RoutesEnum";
-import { Main } from "./app/Main/Main";
-import SignIn from "./features/SignIn/SignIn";
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import { PrivacyPolicy } from './app/PrivacyPolicy/PrivacyPolicy';
+import { RoutesEnum } from './utils/RoutesEnum';
+import { Main } from './app/Main/Main';
+import SignIn from './features/SignIn/SignIn';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path={RoutesEnum.Main} element={<Main />} />
-        <Route index element={<Navigate to={RoutesEnum.PrivacyPolicy} />} />
+        <Route index path={RoutesEnum.Main} element={<Main />} />
         <Route path={RoutesEnum.PrivacyPolicy} element={<PrivacyPolicy />} />
         <Route path={RoutesEnum.Login} element={<SignIn />} />
-
       </Routes>
     </div>
   );

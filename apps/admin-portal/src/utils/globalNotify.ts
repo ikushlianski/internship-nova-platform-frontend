24b63647a7463fbb684e8bd19181b7f9b841dfae
Nikita8Sannikov <Ak-4t7@mail.ru>
2024-09-09 17:47:58 +0600

    make useNotify global to use it outside react components

---
+++ apps/admin-portal/src/utils/globalNotify.ts
@@ -0,0 +1,17 @@
+let notificationSetter: (( message: string, options: {type: 'success' | 'error' | 'info' | 'warning', autoHideDuration?: number }) => void) | null = null;
+
+export const setNotificationFunction = (fn: typeof notificationSetter) => {
+  notificationSetter = fn;
+};
+
+export const showNotify = (
+  message: string,
+  options: { type: "success" | "error" | "warning" | "info",
+  autoHideDuration?: number }
+) => {
+  if (notificationSetter) {
+    notificationSetter(message, options );
+  } else {
+    console.warn("Notify function is not initialized");
+  }
+};

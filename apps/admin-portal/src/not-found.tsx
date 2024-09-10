2383b1c813b4a8628d515821f7ef1038f9342a97
Gor-Hoveyan <gorpro2004@mail.ru>
2024-09-10 07:16:27 +0400

    fix: merge conflicts

--- apps/admin-portal/src/not-found.tsx
+++ apps/admin-portal/src/not-found.tsx
@@ -1,4 +1,4 @@
-import { ErrorContainer, ErrorMessage, ErrorDescription } from "@repo/ui/error";
+import { ErrorContainer, ErrorDescription, ErrorMessage } from "@repo/ui/error";
 import { Title } from "react-admin";
 
 export default function NotFound() {

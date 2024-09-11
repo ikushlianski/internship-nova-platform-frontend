145be54ca5b6b95672a6503f1efe2adbb4881eca
Alexey <a.yemelyanovfe@gmail.com>
2024-09-11 23:32:02 +0300

    add: add test section Header with language switcher

--- apps/spaced-rep-tool-web/src/components/Header/Header.tsx
+++ apps/spaced-rep-tool-web/src/components/Header/Header.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import { LanguageSwitcher } from '../../../../../packages/ui/src/LangugeSwitcher/LangugeSwitcher.component';
+import { LanguageSwitcher } from '@repo/ui/languageSwitcher';
+
 export interface HeaderProps {}
 
-export function Header(props: HeaderProps) {
+export function Header() {
   return (
     <div className="flex items-end">
-      <LanguageSwitcher />
+      <LanguageSwitcher{...props} />
     </div>
   );
 }
--- apps/spaced-rep-tool-web/src/main.tsx
+++ apps/spaced-rep-tool-web/src/main.tsx
@@ -1,33 +1,32 @@
-
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.tsx';
-import "./globals.css";
-
+import './globals.css';
+import './i18n';
 
 async function enableMocking() {
-  if (import.meta.env.VITE_API_MOCKING !== "true") {
+  if (import.meta.env.VITE_API_MOCKING !== 'true') {
     return;
   }
 
-  const { worker } = await import("./mocks/browser");
+  const { worker } = await import('./mocks/browser');
 
   return worker.start();
 }
 
 enableMocking().then(() => {
-  ReactDOM.createRoot(document.getElementById("root")!).render(
+  ReactDOM.createRoot(document.getElementById('root')!).render(
     import.meta.env.APP_ENV === 'production' ? (
       <Router>
-			<App />
-		</Router>              
+        <App />
+      </Router>
     ) : (
-    <React.StrictMode>
-			<Router>
-				<App />
-			</Router>
-		</React.StrictMode>
-  )
+      <React.StrictMode>
+        <Router>
+          <App />
+        </Router>
+      </React.StrictMode>
+    ),
   );
 });

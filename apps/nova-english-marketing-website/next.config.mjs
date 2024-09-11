d490eadfd72bcc88ac931dd533f7aaa48aeca26a
Yury Kinchin <yury.kinchyn@gmail.com>
2024-09-11 14:19:50 +0300

    MWS-32: remove comments

--- apps/nova-english-marketing-website/next.config.mjs
+++ apps/nova-english-marketing-website/next.config.mjs
@@ -1,27 +1,18 @@
 /** @type {import('next').NextConfig} */
 
-import createNextIntlPlugin from "next-intl/plugin"
+import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin()
+const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
-	// async redirects() {
-	// 	return [
-	// 		{
-	// 			source: "/",
-	// 			destination: "/english-for-tech-b1-b2-level",
-	// 			permanent: false
-	// 		}
-	// 	]
-	// },
-	webpack(config) {
-		config.module.rules.push({
-			test: /\.svg$/i,
-			use: ["@svgr/webpack"]
-		})
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
 
-		return config
-	}
-}
+    return config;
+  },
+};
 
-export default withNextIntl(nextConfig)
+export default withNextIntl(nextConfig);

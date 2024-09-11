f3bec28cd7f239377c1b4d8c0ec8bc023ae0d07e
Yury Kinchin <yury.kinchyn@gmail.com>
2024-09-11 17:55:57 +0300

    MWS-32: implement internationalization in 404 page

--- apps/nova-english-marketing-website/src/app/not-found.tsx
+++ apps/nova-english-marketing-website/src/app/not-found.tsx
@@ -1,19 +1,13 @@
-import NotFound from "@/features/NotFound/ui/NotFound.component"
+'use client';
 
-const NotFoundPage = () => {
-	return (
-		<html>
-			<body>
-				<div className="pt-16 flex flex-col justify-center items-center gap-16">
-					<h1 className="text-xl font-bold">
-						Unfortunately, the page you are looking for does not exist or has
-						been moved.
-					</h1>
-					<NotFound />
-				</div>
-			</body>
-		</html>
-	)
+import Error from 'next/error';
+
+export default function NotFound() {
+  return (
+    <html lang="en">
+      <body>
+        <Error statusCode={404} />
+      </body>
+    </html>
+  );
 }
-
-export default NotFoundPage

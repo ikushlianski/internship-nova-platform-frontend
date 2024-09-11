8722bbf10881d63992b777a4ecaded09642b10bb
Nikita8Sannikov <Ak-4t7@mail.ru>
2024-09-11 15:57:05 +0600

    add authProvider

--- apps/admin-portal/src/mocks/dataProvider.ts
+++ apps/admin-portal/src/mocks/dataProvider.ts
@@ -1,13 +1,14 @@
 import { CreateParams, CreateResult, DataProvider, DeleteParams, DeleteResult, Identifier, RaRecord } from "react-admin"
 import { updateResourceData, getDataForResource, generateNewId } from '../utils/Utils'
-import  {IUser}  from '../types/User';
-import  {IStudent}  from '../types/Student';
+import { IUser } from '../types/User';
+import { IStudent } from '../types/Student';
 import { ITeacher } from "../types/Teacher";
 import { IAdmin } from "../types/Admin";
 import { IManager } from "../types/Manager";
 import { ISales } from "../types/Sales";
 import { ISpectator } from "../types/Spectator";
 import { INoRole } from "../types/NoRole";
+import { handleErrorResponse } from "../utils/Utils";
 
 type ResourceTypes = IUser | IStudent | ITeacher | IAdmin | IManager | ISales | ISpectator | INoRole;
 
@@ -16,151 +17,267 @@ const dataProvider: DataProvider = {
         const { page = 1, perPage = 10 } = params.pagination || {}
         const { field = 'id', order = 'ASC' || 'DESC' } = params.sort || {}
 
-        const data = getDataForResource(resource)
+        try {
+            const response = await fetch(`/api/${resource}?page=${page}&perPage=${perPage}`);
 
-        const total = data.length
-        const sortedData = [...data].sort((a, b) => {
-            if (a[field] < b[field]) return order === 'ASC' ? -1 : 1;
-            if (a[field] > b[field]) return order === 'ASC' ? 1 : -1;
-            return 0;
-        })
+            if (!response.ok) {
+                await handleErrorResponse(response);
+            }
 
-        const paginatedData = sortedData.slice((page - 1) * perPage, page * perPage)
+            const data = getDataForResource(resource)
 
-        return {
-            data: paginatedData,
-            total,
+            const total = data.length
+            const sortedData = [...data].sort((a, b) => {
+                if (a[field] < b[field]) return order === 'ASC' ? -1 : 1;
+                if (a[field] > b[field]) return order === 'ASC' ? 1 : -1;
+                return 0;
+            })
+
+            const paginatedData = sortedData.slice((page - 1) * perPage, page * perPage)
+
+            return {
+                data: paginatedData,
+                total,
+            }
+        } catch (error) {
+            console.error(error);
+            throw error;
         }
     },
     getOne: async (resource, params) => {
-        const data = getDataForResource(resource);
-        const item = data.find((item: { id: number }) =>String(item.id) == params.id)
-        if (!item) {
-            throw new Error(`Resource ${resource} with ID ${params.id} not found`)
-        }
+        try {
+            const response = await fetch(`/api/${resource}/${params.id}`);
 
-        return {
-            data: item
+            if (!response.ok) {
+                await handleErrorResponse(response);
+            }
+            const data = getDataForResource(resource);
+            const item = data.find((item: { id: number }) => String(item.id) == params.id)
+            if (!item) {
+                throw new Error(`Resource ${resource} with ID ${params.id} not found`)
+            }
+
+            return {
+                data: item
+            }
+        } catch (error) {
+            console.error(error);
+            throw error;
         }
     },
     getMany: async (resource, params) => {
-        const data = getDataForResource(resource);
-        const items = data.filter((item: { id: number }) =>{
+        try {
+            const response = await fetch(`/api/${resource}/${params.ids.join(', ')}`);
 
-         return params.ids.includes(item.id)
-        });
+            if (!response.ok) {
+                await handleErrorResponse(response);
+            }
+            const data = getDataForResource(resource);
+            const items = data.filter((item: { id: number }) => {
 
-        if (items.length === 0) {
-            throw new Error(`No resources found for IDs: ${params.ids.join(', ')}`);
+                return params.ids.includes(item.id)
+            });
+
+            if (items.length === 0) {
+                throw new Error(`No resources found for IDs: ${params.ids.join(', ')}`);
+            }
+
+            return {
+                data: items
+            };
+        } catch (error) {
+            console.error(error);
+            throw error;
         }
-
-        return {
-            data: items
-        };
     },
     getManyReference: async (resource, params) => {
-        const { target, id } = params;
-        const { page = 1, perPage = 10 } = params.pagination || {};
-        const { field = 'id', order = 'ASC' || 'DESC' } = params.sort || {};
+        try {
+            const response = await fetch(`/api/${resource}?${params.target}=${params.id}&page=${params.pagination?.page}&perPage=${params.pagination?.perPage}`);
 
-        const data = getDataForResource(resource);
+            if (!response.ok) {
+                await handleErrorResponse(response);
+            }
+            const { target, id } = params;
+            const { page = 1, perPage = 10 } = params.pagination || {};
+            const { field = 'id', order = 'ASC' || 'DESC' } = params.sort || {};
 
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const filteredData = data.filter((item: any) => item[target] === id);
-        const total = filteredData.length;
+            const data = getDataForResource(resource);
 
-        const sortedData = [...filteredData].sort((a, b) => {
-            if (a[field] < b[field]) return order === 'ASC' ? -1 : 1;
-            if (a[field] > b[field]) return order === 'ASC' ? 1 : -1;
-            return 0;
-        });
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            const filteredData = data.filter((item: any) => item[target] === id);
+            const total = filteredData.length;
 
-        const paginatedData = sortedData.slice((page - 1) * perPage, page * perPage);
+            const sortedData = [...filteredData].sort((a, b) => {
+                if (a[field] < b[field]) return order === 'ASC' ? -1 : 1;
+                if (a[field] > b[field]) return order === 'ASC' ? 1 : -1;
+                return 0;
+            });
 
-        return {
-            data: paginatedData,
-            total,
-        };
+            const paginatedData = sortedData.slice((page - 1) * perPage, page * perPage);
+
+            return {
+                data: paginatedData,
+                total,
+            };
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
     },
 
     updateMany: async (resource, params) => {
-        const data = getDataForResource(resource);
-        const updatedData = data.map((item) => {
-            if (params.ids.includes(item.id)) {
-                return { ...item, ...params.data };
+        try {
+            const response = await fetch(`/api/${resource}/${params.ids}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    ids: params.ids,
+                    data: params.data,
+                }),
+            });
+
+            if (!response.ok) {
+                await handleErrorResponse(response);
             }
-            return item;
-        });
+            const data = getDataForResource(resource);
+            const updatedData = data.map((item) => {
+                if (params.ids.includes(item.id)) {
+                    return { ...item, ...params.data };
+                }
+                return item;
+            });
 
-        updateResourceData(resource, updatedData);
+            updateResourceData(resource, updatedData);
 
-        return {
-            data: params.ids,
-        };
+            return {
+                data: params.ids,
+            };
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
     },
     deleteMany: async (resource, params) => {
-        const data = getDataForResource(resource);
-        const filteredData = data.filter(item => !params.ids.includes(item.id));
+        try {
+            const response = await fetch(`/api/${resource}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ ids: params.ids }),
+            });
 
-        updateResourceData(resource, filteredData);
+            if (!response.ok) {
+                await handleErrorResponse(response);
+            }
+            const data = getDataForResource(resource);
+            const filteredData = data.filter(item => !params.ids.includes(item.id));
 
-        return {
-            data: params.ids,
-        };
+            updateResourceData(resource, filteredData);
+
+            return {
+                data: params.ids,
+            };
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
     },
 
     create: async <T extends Omit<ResourceTypes, "id">>(resource: string, params: CreateParams<T>): Promise<CreateResult<T & { id: Identifier }>> => {
-        const data = getDataForResource(resource);
-        const newId = generateNewId(data);
-        const newItem = { id: newId, ...params.data } as T & { id: Identifier };
-        const updatedData = [...data, newItem];
-        updateResourceData(resource, updatedData);
+        try {
+            const response = await fetch(`/api/${resource}`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(params.data),
+            });
 
-        return {
-            data: newItem,
-        };
+            if (!response.ok) {
+                await handleErrorResponse(response);
+            }
+            const data = getDataForResource(resource);
+            const newId = generateNewId(data);
+            const newItem = { id: newId, ...params.data } as T & { id: Identifier };
+            const updatedData = [...data, newItem];
+            updateResourceData(resource, updatedData);
+
+            return {
+                data: newItem,
+            };
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
     },
 
     update: async (resource, params) => {
-        const data = getDataForResource(resource);
-        const itemIndex = data.findIndex((item: { id: number }) => String(item.id) === params.id);
+        try {
+            const response = await fetch(`/api/${resource}/${params.id}`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(params.data),
+            });
 
-        if (itemIndex === -1) {
-            throw new Error(`Resource ${resource} with ID ${params.id} not found`);
+            if (!response.ok) {
+                await handleErrorResponse(response);
+            }
+            const data = getDataForResource(resource);
+            const itemIndex = data.findIndex((item: { id: number }) => String(item.id) === params.id);
+
+            if (itemIndex === -1) {
+                throw new Error(`Resource ${resource} with ID ${params.id} not found`);
+            }
+
+            const updatedItem = { ...data[itemIndex], ...params.data };
+            const updatedData = [
+                ...data.slice(0, itemIndex),
+                updatedItem,
+                ...data.slice(itemIndex + 1),
+            ];
+            updateResourceData(resource, updatedData);
+
+            return {
+                data: updatedItem,
+            };
+        } catch (error) {
+            console.error(error);
+            throw error;
         }
-
-        const updatedItem = { ...data[itemIndex], ...params.data };
-        const updatedData = [
-            ...data.slice(0, itemIndex),
-            updatedItem,
-            ...data.slice(itemIndex + 1),
-        ];
-        updateResourceData(resource, updatedData);
-
-        return {
-            data: updatedItem,
-        };
     },
 
     delete: async <RecordType extends RaRecord>(resource: string, params: DeleteParams<RecordType>): Promise<DeleteResult<RecordType>> => {
-        const data = getDataForResource(resource);
-        const itemIndex = data.findIndex((item: RecordType) => item.id === params.id);
+        try {
+            const response = await fetch(`/api/${resource}/${params.id}`, {
+                method: 'DELETE',
+            });
 
-        if (itemIndex === -1) {
-            throw new Error(`Resource ${resource} with ID ${params.id} not found`);
+            if (!response.ok) {
+                await handleErrorResponse(response);
+            }
+            const data = getDataForResource(resource);
+            const itemIndex = data.findIndex((item: RecordType) => item.id === params.id);
+
+            if (itemIndex === -1) {
+                throw new Error(`Resource ${resource} with ID ${params.id} not found`);
+            }
+
+            const updatedData = [
+                ...data.slice(0, itemIndex),
+                ...data.slice(itemIndex + 1),
+            ];
+
+            updateResourceData(resource, updatedData);
+
+            return {
+                data: { id: params.id } as RecordType,
+            };
+        } catch (error) {
+            console.error(error);
+            throw error;
         }
-
-        const updatedData = [
-            ...data.slice(0, itemIndex),
-            ...data.slice(itemIndex + 1),
-        ];
-
-        updateResourceData(resource, updatedData);
-
-        return {
-            data: { id: params.id } as RecordType,
-        };
     },
-  };
+};
 
 export default dataProvider;
--- apps/admin-portal/src/utils/Utils.ts
+++ apps/admin-portal/src/utils/Utils.ts
@@ -3,95 +3,96 @@ import { IUser } from "../types/User"
 import { Users, Students, Teachers, Paths, Classes, Admins, Managers, Sales, Spectators, NoRoles } from '../mocks/data/entities.json'
 import { HttpResponse } from 'msw'
 import { showNotify } from "./globalNotify"
+import { authProvider } from "./authProvider"
 
 // A user can have multiple roles in ecosystem applications, with
 // the most powerful role being dominant.
 type Role = 'Admin' | 'Manager' | 'Sales' | 'Teacher' | 'Student' | 'Spectator' | 'No role'
 
 export function getPrimaryRole(user: IUser): string {
-    const rolePriority: Record<Role, number> = {
-        'Admin': 6,
-        'Manager': 5,
-        'Sales': 4,
-        'Teacher': 3,
-        'Student': 2,
-        'Spectator': 1,
-        'No role': 0
-    };
+  const rolePriority: Record<Role, number> = {
+    'Admin': 6,
+    'Manager': 5,
+    'Sales': 4,
+    'Teacher': 3,
+    'Student': 2,
+    'Spectator': 1,
+    'No role': 0
+  };
 
-    return user.roles.reduce((prev, curr) => {
-        return rolePriority[curr] > rolePriority[prev] ? curr : prev;
-    })
+  return user.roles.reduce((prev, curr) => {
+    return rolePriority[curr] > rolePriority[prev] ? curr : prev;
+  })
 }
 
 //Function for gettin data from `entities.json` file
 export const getDataForResource = (resource: string): any[] => {
-    switch (resource) {
-        case 'users':
-            return Users;
-        case 'students':
-            return Students;
-        case 'teachers':
-            return Teachers;
-        case 'paths':
-            return Paths;
-        case 'classes':
-            return Classes;
-        case 'admins':
-            return Admins;
-        case 'managers':
-            return Managers;
-        case 'sales':
-            return Sales;
-        case 'spectators':
-            return Spectators;
-        case 'noRoles':
-            return NoRoles;
-        default:
-            throw new Error(`Unknown resource: ${resource}`);
-    }
+  switch (resource) {
+    case 'users':
+      return Users;
+    case 'students':
+      return Students;
+    case 'teachers':
+      return Teachers;
+    case 'paths':
+      return Paths;
+    case 'classes':
+      return Classes;
+    case 'admins':
+      return Admins;
+    case 'managers':
+      return Managers;
+    case 'sales':
+      return Sales;
+    case 'spectators':
+      return Spectators;
+    case 'noRoles':
+      return NoRoles;
+    default:
+      throw new Error(`Unknown resource: ${resource}`);
+  }
 }
 
 //Function for "UpdateMany "to update data in the `entities.json` file
 export const updateResourceData = (resource: string, updatedData: any[]): void => {
-    switch (resource) {
-        case 'users':
-            Users.splice(0, Users.length, ...updatedData)
-            break
-        case 'students':
-            Students.splice(0, Students.length, ...updatedData)
-            break
-        case 'teachers':
-            Teachers.splice(0, Teachers.length, ...updatedData)
-            break
-        case 'paths':
-            Paths.splice(0, Paths.length, ...updatedData)
-            break
-        case 'classes':
-            Classes.splice(0, Classes.length, ...updatedData)
-            break
-        case 'admins':
-            Admins.splice(0, Admins.length, ...updatedData)
-            break
-        case 'managers':
-            Managers.splice(0, Managers.length, ...updatedData)
-            break
-        case 'sales':
-            Sales.splice(0, Sales.length, ...updatedData)
-            break
-        case 'spectators':
-            Spectators.splice(0, Spectators.length, ...updatedData)
-            break
-        case 'noRoles':
-            NoRoles.splice(0, NoRoles.length, ...updatedData)
-            break
-        default:
-            throw new Error(`Unknown resource: ${resource}`)
-    }
+  switch (resource) {
+    case 'users':
+      Users.splice(0, Users.length, ...updatedData)
+      break
+    case 'students':
+      Students.splice(0, Students.length, ...updatedData)
+      break
+    case 'teachers':
+      Teachers.splice(0, Teachers.length, ...updatedData)
+      break
+    case 'paths':
+      Paths.splice(0, Paths.length, ...updatedData)
+      break
+    case 'classes':
+      Classes.splice(0, Classes.length, ...updatedData)
+      break
+    case 'admins':
+      Admins.splice(0, Admins.length, ...updatedData)
+      break
+    case 'managers':
+      Managers.splice(0, Managers.length, ...updatedData)
+      break
+    case 'sales':
+      Sales.splice(0, Sales.length, ...updatedData)
+      break
+    case 'spectators':
+      Spectators.splice(0, Spectators.length, ...updatedData)
+      break
+    case 'noRoles':
+      NoRoles.splice(0, NoRoles.length, ...updatedData)
+      break
+    default:
+      throw new Error(`Unknown resource: ${resource}`)
+  }
 }
 
 export const generateNewId = (data: any[]): number => {
-    return data.length ? Math.max(...data.map(item => item.id)) + 1 : 1;
+  return data.length ? Math.max(...data.map(item => item.id)) + 1 : 1;
 };
 
 //tailwind classes for Toast
@@ -103,13 +104,13 @@ export const toastClasses: Record<string, string> = {
 };
 
 // GET request function
-export const handleGetRequest = <T>(collection: T[], map:  Map<number, T>, entityName: string) =>{
+export const handleGetRequest = <T>(collection: T[], map: Map<number, T>, entityName: string) => {
   showNotify(`${entityName} successfully loaded!`, { type: "success", autoHideDuration: 1500 })
   return HttpResponse.json([...collection, ...map]);
 }
 
 // GET by Id request function
-export const handleGetIdRequest =<T extends { id: number }>( params: { id?: string }, collection: T[], entityName: string) => {
+export const handleGetIdRequest = <T extends { id: number }>(params: { id?: string }, collection: T[], entityName: string) => {
   const id = params.id ? Number(params.id) : null;
 
   const response = id ? collection.find(item => item.id === id) : [...collection];
@@ -129,16 +130,16 @@ export const handlePostRequest = async <T>(request: Request, collection: Map<num
     const newItem = await request.json() as T & { id: number };
 
     if (!newItem || typeof newItem.id !== 'number') {
-        return HttpResponse.json({ error: `Invalid ${entityName} data` }, { status: 400 })
+      return HttpResponse.json({ error: `Invalid ${entityName} data` }, { status: 400 })
     }
 
     collection.set(newItem.id, newItem)
     showNotify(`${entityName} successfully loaded!`, { type: "success", autoHideDuration: 3000 })
     return HttpResponse.json(newItem, { status: 201 })
-} catch (error) {
+  } catch (error) {
     showNotify("Failed to parse request body", { type: "error", autoHideDuration: 3000 })
     return HttpResponse.json({ error: 'Failed to parse request body' }, { status: 400 })
-}
+  }
 }
 
 // PUT request function
@@ -148,19 +149,19 @@ export const handlePutRequest = async<T extends { id: number }>(params: { id?: s
     const updatedItem = await request.json() as T & { id: number };
 
     if (!updatedItem || typeof updatedItem.id !== 'number') {
-        return HttpResponse.json({ error: `Invalid ${entityName} data` }, { status: 400 })
+      return HttpResponse.json({ error: `Invalid ${entityName} data` }, { status: 400 })
     }
 
-     collection  = collection.map(item => item.id === Number(id) ? updatedItem : item)
+    collection = collection.map(item => item.id === Number(id) ? updatedItem : item)
     if (map.has(Number(id))) {
-        map.set(Number(id), updatedItem);
-      }
-      showNotify(`${entityName} with ID ${id} successfully updated`, { type: "success", autoHideDuration: 3000 })
+      map.set(Number(id), updatedItem);
+    }
+    showNotify(`${entityName} with ID ${id} successfully updated`, { type: "success", autoHideDuration: 3000 })
     return HttpResponse.json(updatedItem)
-} catch (error) {
-  showNotify(`'Failed to parse request body'`, { type: "error", autoHideDuration: 3000 })
+  } catch (error) {
+    showNotify(`'Failed to parse request body'`, { type: "error", autoHideDuration: 3000 })
     return HttpResponse.json({ error: 'Failed to parse request body' }, { status: 400 })
-}
+  }
 }
 
 // DELETE request function
@@ -180,4 +181,14 @@ export const parseToNumberArray = (input: any) => {
     return input.map((id: any) => Number(id));
   }
   return typeof input !== "undefined" ? [Number(input)] : [];
+};
+
+//function for initializing authProvider.checkError({ status: 401 })
+export const handleErrorResponse = async (response: Response) => {
+  if (response.status === 401) {
+    await authProvider.checkError({ status: 401 });
+    throw new Error('Unauthorized');
+  }
+  const error = await response.json();
+  throw new Error(error.message || 'Unknown Error');
 };

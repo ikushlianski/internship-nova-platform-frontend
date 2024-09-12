1a4958d87135285be9d562d6fbfe7887def4d5f6
Yury Kinchin <yury.kinchyn@gmail.com>
2024-09-12 14:03:36 +0300

    MWS-32: formate by prettier

--- apps/nova-english-marketing-website/src/app/[locale]/class/[class-title]/page.tsx
+++ apps/nova-english-marketing-website/src/app/[locale]/class/[class-title]/page.tsx
@@ -1,17 +1,17 @@
-import { useTranslations } from "next-intl"
-import { unstable_setRequestLocale } from "next-intl/server"
+import { useTranslations } from 'next-intl';
+import { unstable_setRequestLocale } from 'next-intl/server';
 
 type Props = {
-	params: { locale: string }
-}
+  params: { locale: string };
+};
 
 export default function Page({ params: { locale } }: Props) {
-	unstable_setRequestLocale(locale)
+  unstable_setRequestLocale(locale);
 
-	const t = useTranslations("temporary")
-	return (
-		<div>
-			<h1>{t("b1_b2_level_course")}</h1>
-		</div>
-	)
+  const t = useTranslations('temporary');
+  return (
+    <div>
+      <h1>{t('b1_b2_level_course')}</h1>
+    </div>
+  );
 }

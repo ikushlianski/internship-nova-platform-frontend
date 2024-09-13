e1e742ee8ade74f1652c28cb614f17d7b94890f9
Nikita8Sannikov <Ak-4t7@mail.ru>
2024-09-13 10:26:05 +0600

    refactor notification duration

--- apps/admin-portal/src/utils/Utils.ts
+++ apps/admin-portal/src/utils/Utils.ts
@@ -1,124 +1,120 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { IUser } from "../types/User"
 import { Users, Students, Teachers, Paths, Classes, Admins, Managers, Sales, Spectators, NoRoles } from '../mocks/data/entities.json'
 import { HttpResponse } from 'msw'
 import { showNotify } from "./globalNotify"
+import { UserRole as Role } from '@repo/shared-types/user'
+import { User } from '@repo/shared-types/user'
 
 // A user can have multiple roles in ecosystem applications, with
 // the most powerful role being dominant.
-type Role = 'Admin' | 'Manager' | 'Sales' | 'Teacher' | 'Student' | 'Spectator' | 'No role'
 
-export function getPrimaryRole(user: IUser): string {
-    const rolePriority: Record<Role, number> = {
-        'Admin': 6,
-        'Manager': 5,
-        'Sales': 4,
-        'Teacher': 3,
-        'Student': 2,
-        'Spectator': 1,
-        'No role': 0
-    };
+export function getPrimaryRole(user: User): string {
+  const rolePriority: Partial<Record<Role, number>> = {
+    [Role.Admin]: 6,
+    [Role.Manager]: 5,
+    [Role.Sale]: 4,
+    [Role.Teacher]: 3,
+    [Role.Student]: 2,
+    [Role.Spectator]: 1,
+    [Role.NoRole]: 0,
+  };
 
-    return user.roles.reduce((prev, curr) => {
-        return rolePriority[curr] > rolePriority[prev] ? curr : prev;
-    })
+  return user.roles.reduce((prev, curr) => {
+    return (rolePriority[curr] || 0) > (rolePriority[prev] || 0) ? curr : prev;
+  })
 }
 
 //Function for gettin data from `entities.json` file
 export const getDataForResource = (resource: string): any[] => {
-    switch (resource) {
-        case 'users':
-            return Users;
-        case 'students':
-            return Students;
-        case 'teachers':
-            return Teachers;
-        case 'paths':
-            return Paths;
-        case 'classes':
-            return Classes;
-        case 'admins':
-            return Admins;
-        case 'managers':
-            return Managers;
-        case 'sales':
-            return Sales;
-        case 'spectators':
-            return Spectators;
-        case 'noRoles':
-            return NoRoles;
-        default:
-            throw new Error(`Unknown resource: ${resource}`);
-    }
+  switch (resource) {
+    case 'users':
+      return Users;
+    case 'students':
+      return Students;
+    case 'teachers':
+      return Teachers;
+    case 'paths':
+      return Paths;
+    case 'classes':
+      return Classes;
+    case 'admins':
+      return Admins;
+    case 'managers':
+      return Managers;
+    case 'sales':
+      return Sales;
+    case 'spectators':
+      return Spectators;
+    case 'noroles':
+      return NoRoles;
+    default:
+      throw new Error(`Unknown resource: ${resource}`);
+  }
 }
 
 //Function for "UpdateMany "to update data in the `entities.json` file
 export const updateResourceData = (resource: string, updatedData: any[]): void => {
-    switch (resource) {
-        case 'users':
-            Users.splice(0, Users.length, ...updatedData)
-            break
-        case 'students':
-            Students.splice(0, Students.length, ...updatedData)
-            break
-        case 'teachers':
-            Teachers.splice(0, Teachers.length, ...updatedData)
-            break
-        case 'paths':
-            Paths.splice(0, Paths.length, ...updatedData)
-            break
-        case 'classes':
-            Classes.splice(0, Classes.length, ...updatedData)
-            break
-        case 'admins':
-            Admins.splice(0, Admins.length, ...updatedData)
-            break
-        case 'managers':
-            Managers.splice(0, Managers.length, ...updatedData)
-            break
-        case 'sales':
-            Sales.splice(0, Sales.length, ...updatedData)
-            break
-        case 'spectators':
-            Spectators.splice(0, Spectators.length, ...updatedData)
-            break
-        case 'noRoles':
-            NoRoles.splice(0, NoRoles.length, ...updatedData)
-            break
-        default:
-            throw new Error(`Unknown resource: ${resource}`)
-    }
+  switch (resource) {
+    case 'users':
+      Users.splice(0, Users.length, ...updatedData)
+      break
+    case 'students':
+      Students.splice(0, Students.length, ...updatedData)
+      break
+    case 'teachers':
+      Teachers.splice(0, Teachers.length, ...updatedData)
+      break
+    case 'paths':
+      Paths.splice(0, Paths.length, ...updatedData)
+      break
+    case 'classes':
+      Classes.splice(0, Classes.length, ...updatedData)
+      break
+    case 'admins':
+      Admins.splice(0, Admins.length, ...updatedData)
+      break
+    case 'managers':
+      Managers.splice(0, Managers.length, ...updatedData)
+      break
+    case 'sales':
+      Sales.splice(0, Sales.length, ...updatedData)
+      break
+    case 'spectators':
+      Spectators.splice(0, Spectators.length, ...updatedData)
+      break
+    case 'noroles':
+      NoRoles.splice(0, NoRoles.length, ...updatedData)
+      break
+    default:
+      throw new Error(`Unknown resource: ${resource}`)
+  }
 }
 
 export const generateNewId = (data: any[]): number => {
-    return data.length ? Math.max(...data.map(item => item.id)) + 1 : 1;
+  return data.length ? Math.max(...data.map(item => item.id)) + 1 : 1;
 };
 
-//tailwind classes for Toast
-export const toastClasses: Record<string, string> = {
-  success: "bg-green-500 text-white",
-  error: "bg-red-500 text-white",
-  warning: "bg-yellow-500 text-white",
-  info: "bg-blue-500 text-white",
-};
-
+const NOTIFICATION_DISPLAY_DURATION = {
+  long: 3000,
+  fast: 1500
+}
 // GET request function
-export const handleGetRequest = <T>(collection: T[], map:  Map<number, T>, entityName: string) =>{
-  showNotify(`${entityName} successfully loaded!`, { type: "success", autoHideDuration: 1500 })
+export const handleGetRequest = <T>(collection: T[], map: Map<number, T>, entityName: string) => {
+  showNotify(`${entityName} successfully loaded!`, { type: "success", autoHideDuration: NOTIFICATION_DISPLAY_DURATION.fast })
   return HttpResponse.json([...collection, ...map]);
 }
 
 // GET by Id request function
-export const handleGetIdRequest =<T extends { id: number }>( params: { id?: string }, collection: T[], entityName: string) => {
+export const handleGetIdRequest = <T extends { id: number }>(params: { id?: string }, collection: T[], entityName: string) => {
   const id = params.id ? Number(params.id) : null;
 
   const response = id ? collection.find(item => item.id === id) : [...collection];
 
   if (response) {
-    showNotify(`${entityName} with ID ${id} successfully found!`, { type: "success", autoHideDuration: 3000 });
+    showNotify(`${entityName} with ID ${id} successfully found!`, { type: "success", autoHideDuration: NOTIFICATION_DISPLAY_DURATION.long });
     return HttpResponse.json(response);
   } else {
-    showNotify(`${entityName} with ID ${id} not found!`, { type: "error", autoHideDuration: 3000 });
+    showNotify(`${entityName} with ID ${id} not found!`, { type: "error", autoHideDuration: NOTIFICATION_DISPLAY_DURATION.long });
     return HttpResponse.json(null, { status: 404 })
   }
 }
@@ -129,16 +125,16 @@ export const handlePostRequest = async <T>(request: Request, collection: Map<num
     const newItem = await request.json() as T & { id: number };
 
     if (!newItem || typeof newItem.id !== 'number') {
-        return HttpResponse.json({ error: `Invalid ${entityName} data` }, { status: 400 })
+      return HttpResponse.json({ error: `Invalid ${entityName} data` }, { status: 400 })
     }
 
     collection.set(newItem.id, newItem)
-    showNotify(`${entityName} successfully loaded!`, { type: "success", autoHideDuration: 3000 })
+    showNotify(`${entityName} successfully loaded!`, { type: "success", autoHideDuration: NOTIFICATION_DISPLAY_DURATION.long })
     return HttpResponse.json(newItem, { status: 201 })
-} catch (error) {
-    showNotify("Failed to parse request body", { type: "error", autoHideDuration: 3000 })
+  } catch (error) {
+    showNotify("Failed to parse request body", { type: "error", autoHideDuration: NOTIFICATION_DISPLAY_DURATION.long })
     return HttpResponse.json({ error: 'Failed to parse request body' }, { status: 400 })
-}
+  }
 }
 
 // PUT request function
@@ -148,26 +144,26 @@ export const handlePutRequest = async<T extends { id: number }>(params: { id?: s
     const updatedItem = await request.json() as T & { id: number };
 
     if (!updatedItem || typeof updatedItem.id !== 'number') {
-        return HttpResponse.json({ error: `Invalid ${entityName} data` }, { status: 400 })
+      return HttpResponse.json({ error: `Invalid ${entityName} data` }, { status: 400 })
     }
 
-     collection  = collection.map(item => item.id === Number(id) ? updatedItem : item)
+    collection = collection.map(item => item.id === Number(id) ? updatedItem : item)
     if (map.has(Number(id))) {
-        map.set(Number(id), updatedItem);
-      }
-      showNotify(`${entityName} with ID ${id} successfully updated`, { type: "success", autoHideDuration: 3000 })
+      map.set(Number(id), updatedItem);
+    }
+    showNotify(`${entityName} with ID ${id} successfully updated`, { type: "success", autoHideDuration: NOTIFICATION_DISPLAY_DURATION.long })
     return HttpResponse.json(updatedItem)
-} catch (error) {
-  showNotify(`'Failed to parse request body'`, { type: "error", autoHideDuration: 3000 })
+  } catch (error) {
+    showNotify(`'Failed to parse request body'`, { type: "error", autoHideDuration: NOTIFICATION_DISPLAY_DURATION.long })
     return HttpResponse.json({ error: 'Failed to parse request body' }, { status: 400 })
-}
+  }
 }
 
 // DELETE request function
 export const handleDeleteRequest = <T extends { id: number }>(params: { id?: string }, collection: T[], entityName: string) => {
   const { id } = params
   collection = collection.filter(item => String(item.id) !== id)
-  showNotify(`${entityName} with ID ${id} successfully deleted`, { type: "success", autoHideDuration: 3000 })
+  showNotify(`${entityName} with ID ${id} successfully deleted`, { type: "success", autoHideDuration: NOTIFICATION_DISPLAY_DURATION.long })
   return HttpResponse.json({ id })
 }
 

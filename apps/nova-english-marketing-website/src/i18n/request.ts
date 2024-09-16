d4b1b933881ff01fe5db09d56fb4c447bb4e1b76
Yury Kinchin <yury.kinchyn@gmail.com>
2024-09-16 13:54:09 +0300

    MWS-32: formate by prettier

--- apps/nova-english-marketing-website/src/i18n/request.ts
+++ apps/nova-english-marketing-website/src/i18n/request.ts
@@ -1,12 +1,12 @@
-import { getRequestConfig } from "next-intl/server"
-import { notFound } from "next/navigation"
-import { routing } from "./routing"
+import { getRequestConfig } from 'next-intl/server';
+import { notFound } from 'next/navigation';
+import { routing } from './routing';
 
 export default getRequestConfig(async ({ locale }) => {
-	// Validate that the incoming `locale` parameter is valid
-	if (!routing.locales.includes(locale as string)) notFound()
+  // Validate that the incoming `locale` parameter is valid
+  if (!routing.locales.includes(locale as string)) notFound();
 
-	return {
-		messages: (await import(`../../messages/${locale}/main.json`)).default
-	}
-})
+  return {
+    messages: (await import(`../../messages/${locale}/main.json`)).default,
+  };
+});

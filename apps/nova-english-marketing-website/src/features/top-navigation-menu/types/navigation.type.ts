358cb6ec06b13406734c56d01853b249960022d5
Yury Kinchin <yury.kinchyn@gmail.com>
2024-09-16 12:02:10 +0300

    MWS-25: change routing endpoints

--- apps/nova-english-marketing-website/src/features/top-navigation-menu/types/navigation.type.ts
+++ apps/nova-english-marketing-website/src/features/top-navigation-menu/types/navigation.type.ts
@@ -1,24 +1,23 @@
-import { ReactNode } from "react"
+import { ReactNode } from 'react';
 
 export enum NavRoutes {
-	MAIN = "main",
-	WHY_US = "why_us",
-	FAQ = "faq",
-	PRICING = "pricing",
-	ENGLISH_ASSESSMENT = "english_level_assessment"
+  MAIN = 'main',
+  WHY_US = 'why_us',
+  FAQ = 'faq',
+  PRICING = 'pricing',
+  CHECK_ENGLISH_LEVEL = 'check_english_level_online',
 }
 
 export const NavPath: Record<NavRoutes, string> = {
-	// Temporary placeholder for main route
-	[NavRoutes.MAIN]: "/english-for-tech-b1-b2-level",
-	[NavRoutes.WHY_US]: "/why-us",
-	[NavRoutes.FAQ]: "/faq",
-	[NavRoutes.PRICING]: "/pricing",
-	[NavRoutes.ENGLISH_ASSESSMENT]: "/english-level-assessment"
-}
+  [NavRoutes.MAIN]: '/',
+  [NavRoutes.WHY_US]: '/why-us',
+  [NavRoutes.FAQ]: '/faq',
+  [NavRoutes.PRICING]: '/pricing',
+  [NavRoutes.CHECK_ENGLISH_LEVEL]: '/check-english-level-online',
+};
 
 export interface NavItem {
-	label: string
-	path: string
-	icon?: ReactNode
+  label: string;
+  path: string;
+  icon?: ReactNode;
 }

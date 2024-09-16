5d9c7b6866682c588eb74e0ca4b843f75dabc4ab
Yury Kinchin <yury.kinchyn@gmail.com>
2024-09-16 12:12:24 +0300

    MWS-25: formate by prettier

--- packages/ui/src/logo/Logo.tsx
+++ packages/ui/src/logo/Logo.tsx
@@ -1,14 +1,14 @@
-import { LogoSVG } from './LogoSVG'
+import { LogoSVG } from './LogoSVG';
 
 interface LogoProps {
-	className?: string
+  className?: string;
 }
 
 export const Logo = ({ className }: LogoProps) => {
-	return (
-		<div className={`${className} flex items-center w-full space-x-3`}>
-			<LogoSVG />
-			<span className='text-primary-blue text-3xl'>NOVA ENGLISH</span>
-		</div>
-	)
-}
+  return (
+    <div className={`${className} flex items-center w-full space-x-3`}>
+      <LogoSVG />
+      <span className="text-primary-blue text-3xl">NOVA ENGLISH</span>
+    </div>
+  );
+};

be8f04696b81b8986ae116cbb354ac4f6157223a
VLAD <buladzislau@gmail.com>
2024-09-19 20:40:45 +0300

    fix: fix comments

--- apps/admin-portal/src/shared/Typography/types.ts
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-type Typography = 'body1' | 'body2' | 'h3' | 'caption2';
-
-export type TypographyProps = {
-  type?: Typography;
-  className?: string;
-} & PropsWithChildren;

203a1657f2c22883febb084dc91ba6cda33e3f3e
Yury Kinchin <yury.kinchyn@gmail.com>
2024-09-19 22:52:06 +0300

    MWS-27: add nda link

--- apps/nova-english-marketing-website/src/app/[locale]/check-english-level-online/online-test/page.tsx
+++ apps/nova-english-marketing-website/src/app/[locale]/check-english-level-online/online-test/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { TestForm } from '@/features/TestForm/ui/TestForm.component';
+import { Link } from '@/i18n/routing';
 import { useTranslations } from 'next-intl';
 
 export default function OnlineTestPage() {
@@ -8,10 +9,14 @@ export default function OnlineTestPage() {
 
   return (
     <section className="px-[250px] py-20 flex gap-16 bg-[radial-gradient(circle,_rgba(183,225,255,1)_0%,_rgba(230,245,255,1)_100%)] rounded-2xl mb-24">
-      <div className="p-20 rounded-[20px] bg-white-foreground">
+      <div className="p-20 rounded-[20px] bg-white-foreground flex flex-col">
         <h1 className="text-[#0092FC] text-5xl font-semibold mb-5">{t('title')}</h1>
         <p className="text-2xl font-medium mb-10">{t('description')}</p>
         <TestForm />
+        <p className="self-center mt-5">
+          <span className="text-[#B8B8B8]"> {t('nda.title')}</span>{' '}
+          <Link href={'/'}>{t('nda.link')}</Link>
+        </p>
       </div>
     </section>
   );

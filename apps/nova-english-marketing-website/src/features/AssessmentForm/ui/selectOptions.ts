20ee9b27615bacb823a2f492f7621377d0185f54
Yury Kinchin <yury.kinchyn@gmail.com>
2024-09-22 19:45:00 +0300

    MWS-27: remove select items

---
+++ apps/nova-english-marketing-website/src/features/AssessmentForm/ui/selectOptions.ts
@@ -0,0 +1,14 @@
+interface SelectOption {
+  value: string;
+  content: string;
+}
+
+export const SelectOptions: SelectOption[] = [
+  { value: 'frontend-dev', content: 'Frontend Developer' },
+  { value: 'backend-dev', content: 'Backend Developer' },
+  { value: 'fullstack-dev', content: 'Full Stack Developer' },
+  { value: 'architect', content: 'Architect' },
+  { value: 'project-manager', content: 'Project Manager' },
+  { value: 'devops', content: 'DevOps Engineer' },
+  { value: 'hr', content: 'HR' },
+];

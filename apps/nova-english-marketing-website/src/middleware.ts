0ce4774a2afbc168e247853031fb82592a06305e
Alexey <a.yemelyanovfe@gmail.com>
2024-09-24 20:32:13 +0300

    fix: fix middleware file

--- apps/nova-english-marketing-website/src/middleware.ts
+++ apps/nova-english-marketing-website/src/middleware.ts
@@ -1,20 +1,18 @@
 import createMiddleware from 'next-intl/middleware';
-import { type NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
+import { type NextRequest, NextResponse } from 'next/server';
 import { routing } from './i18n/routing';
 
 const intlMiddleware = createMiddleware(routing);
 
 const PUBLIC_FILE = /\.(.*)$/;
 
-export function middleware(req: NextRequest): NextResponse {
+export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
+  const response = intlMiddleware(req);
 
   if (PUBLIC_FILE.test(pathname) || pathname.startsWith('/api')) {
     return NextResponse.next();
   }
-
-  const response = intlMiddleware(req);
 
   if (response) {
     return response;

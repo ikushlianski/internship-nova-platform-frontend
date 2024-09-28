2b0ea728d541a7ff47b91ead1038e9de0c3b5634
Yury Kinchin <yury.kinchyn@gmail.com>
2024-09-28 17:03:53 +0300

    MWS-25: add background

--- apps/nova-english-marketing-website/src/features/top-navigation-menu/ui/NavgationMenu.component.tsx
+++ apps/nova-english-marketing-website/src/features/top-navigation-menu/ui/NavgationMenu.component.tsx
@@ -35,26 +35,28 @@ export const NavgationMenu = () => {
   const t = useTranslations('header');
 
   return (
-    <div className="pb-24 flex justify-center">
-      <header className="h-header fixed flex layout items-center justify-between">
-        <Link href={'/'}>
-          <Logo />
-        </Link>
-        <ul className="flex gap-10 xl:gap-52">
-          {navItems.map(({ label, path, icon }) => (
-            <li key={path}>
-              <NavLink label={t(label)} path={path} icon={icon} />
-            </li>
-          ))}
-        </ul>
-
-        <div className="flex items-center">
+    <header className="pb-24">
+      <div className="fixed w-full bg-white-foreground">
+        <div className="h-header flex layout items-center justify-between">
           <Link href={'/'}>
-            <TGLogo className="h-icon" />
+            <Logo />
           </Link>
-          <LocaleSwitcher />
+          <ul className="flex gap-10 xl:gap-52">
+            {navItems.map(({ label, path, icon }) => (
+              <li key={path}>
+                <NavLink label={t(label)} path={path} icon={icon} />
+              </li>
+            ))}
+          </ul>
+
+          <div className="flex items-center">
+            <Link href={'/'}>
+              <TGLogo className="h-icon" />
+            </Link>
+            <LocaleSwitcher />
+          </div>
         </div>
-      </header>
-    </div>
+      </div>
+    </header>
   );
 };

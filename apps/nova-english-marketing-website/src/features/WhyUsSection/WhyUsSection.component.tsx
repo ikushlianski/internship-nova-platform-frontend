e557dfbfecc1f62bea16e620c3b74f1465c6f3b3
mikhaelHan <hancharuk270185@mail.ru>
2024-09-29 20:26:04 +0300

    feat: add titles and images to the section cards

--- apps/nova-english-marketing-website/src/features/WhyUsSection/WhyUsSection.component.tsx
+++ apps/nova-english-marketing-website/src/features/WhyUsSection/WhyUsSection.component.tsx
@@ -4,6 +4,10 @@ import { useTranslations } from 'next-intl';
 const WhyUsSection = () => {
   const t = useTranslations('about');
 
+  const textConfigure = (words: string, start: number, end?: number) => {
+    return words.split(' ').slice(start, end).join(' ');
+  };
+
   return (
     <section className="px-4 py-10 custom-mx">
       <div className="max-w-[1420px] mx-auto mt-10 lg:mt-20">
@@ -18,6 +22,71 @@ const WhyUsSection = () => {
             height={25}
             className="absolute bottom-0 left-1/2 transform -translate-x-1/2 z-0"
           />
+        </div>
+
+        <div className="flex flex-wrap justify-center gap-x-5 gap-y-10">
+          <div className="flex flex-col w-full sm:w-[460px]  sm:items-center">
+            <div className="mb-6">
+              <Image
+                src="./assets/icons/woman-at-computer-flad-and-town-background.svg"
+                alt="woman-at-computer"
+                width={280}
+                height={280}
+                className="object-cover"
+              />
+            </div>
+
+            <div>
+              <h2 className="mb-3 sm:mb-5 text-[#0092FC] font-semibold text-xl sm:text-[40px] leading-tight text-center">
+                <span className="text-[#3f3e3e]">
+                  {textConfigure(t('features.learning_in_english'), 0, 1)}
+                </span>{' '}
+                {textConfigure(t('features.learning_in_english'), 1)}
+              </h2>
+            </div>
+          </div>
+
+          <div className="flex flex-col w-full sm:w-[460px]  sm:items-center">
+            <div className="mb-6">
+              <Image
+                src="./assets/icons/man-paper-airplane.svg"
+                alt="man-paper-airplane"
+                width={280}
+                height={280}
+                className="object-cover"
+              />
+            </div>
+
+            <div>
+              <h2 className="mb-3 sm:mb-5 text-[#0092FC] font-semibold text-xl sm:text-[40px] leading-tight text-center">
+                {textConfigure(t('features.interactive_lessons'), 0, 1)}{' '}
+                <span className="text-[#3f3e3e]">
+                  {textConfigure(t('features.interactive_lessons'), 1)}
+                </span>
+              </h2>
+            </div>
+          </div>
+
+          <div className="flex flex-col w-full sm:w-[460px]  sm:items-center">
+            <div className="mb-6">
+              <Image
+                src="./assets/icons/man-and-woman-standing-near-board.svg"
+                alt="man-and-woman"
+                width={280}
+                height={280}
+                className="object-cover"
+              />
+            </div>
+
+            <div>
+              <h2 className="mb-3 sm:mb-5 text-[#0092FC] font-semibold text-xl sm:text-[40px] leading-tight text-center">
+                <span className="text-[#3f3e3e]">
+                  {textConfigure(t('features.it_teachers'), 0, 1)}
+                </span>{' '}
+                {textConfigure(t('features.it_teachers'), 1)}
+              </h2>
+            </div>
+          </div>
         </div>
       </div>
     </section>

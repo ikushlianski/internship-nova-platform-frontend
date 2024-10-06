c022fc51faedff2dfa2ea3a92cccd77c000a5eca
mikhaelHan <hancharuk270185@mail.ru>
2024-10-07 01:24:49 +0300

    feat: add functionality and adaptability for different devices

--- apps/nova-english-marketing-website/src/features/FAQSection/ui/FAQItem.tsx
+++ apps/nova-english-marketing-website/src/features/FAQSection/ui/FAQItem.tsx
@@ -1,23 +1,45 @@
-import React, { FC } from 'react';
+'use client';
+import React, { FC, useState } from 'react';
 import { IFAQItemProps } from '../types/FAQSection.types';
 
 const FAQItem: FC<IFAQItemProps> = ({ ind, question, answer }) => {
+  const [state, setState] = useState<boolean>(true);
+
+  const handleClick = () => {
+    setState((prevSatate) => !prevSatate);
+  };
+
   return (
     <div>
       <div className="flex justify-between mb-8 pb-8 border-b-2">
-        <div className="text-primary-foreground text-2xl md:text-5xl font-semibold shadow-sm flex">
+        <div className="text-primary-foreground text-2xl sm:text-3xl lg:text-5xl font-semibold shadow-sm flex">
           <p className="mr-5 text-blue-500">{`/0${ind + 1}`} </p>
-          <p>{question}</p>
+          <div>
+            <p>{question}</p>
+            <p className={`${state ? 'hidden' : ''} mt-6 text-base sm:text-xl lg:text-2xl`}>
+              {answer}
+            </p>
+          </div>
         </div>
         <div>
-          <button className="w-8 h-8 md:w-14 md:h-14 rounded-full bg-blue-500 flex justify-center items-center">
+          <button
+            onClick={handleClick}
+            className={`${state ? '' : 'hidden'} w-8 h-8 md:w-14 md:h-14 rounded-full bg-blue-500 flex justify-center items-center`}
+          >
             <span className="inline-block w-8 h-px bg-white-foreground relative">
               <span className="inline-block w-8 h-px bg-white-foreground rotate-90 absolute top-0 left-0"></span>
             </span>
           </button>
+          <button
+            onClick={handleClick}
+            className={`${state ? 'hidden' : ''} w-8 h-8 md:w-14 md:h-14 rounded-full border border-blue-500 flex justify-center items-center`}
+          >
+            <span className="inline-block w-5 md:w-8 h-px bg-blue-500 rotate-45 relative">
+              <span className="inline-block w-5 md:w-8 h-px bg-blue-500 rotate-90 absolute top-0 left-0"></span>
+            </span>
+          </button>
         </div>
       </div>
-      <div className="hidden">{answer}</div>
     </div>
   );
 };
--- apps/nova-english-marketing-website/src/features/FAQSection/ui/FAQSection.tsx
+++ apps/nova-english-marketing-website/src/features/FAQSection/ui/FAQSection.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import { useTranslations } from 'next-intl';
 import FAQItem from './FAQItem';
 
@@ -17,17 +16,23 @@ const FAQSection = () => {
     <section className="px-4 py-10 custom-mx">
       <div className="max-w-[1420px] mx-auto mt-10 lg:mt-20">
         <div className="relative mb-8">
-          <h1 className="text-[#3F3E3E] font-bold text-2xl sm:text-4xl lg:text-6xl leading-tight z-10 relative text-center">
+          <h1 className="text-[#3F3E3E] font-bold text-3xl sm:text-4xl lg:text-6xl leading-tight z-10 relative text-center">
             {t('title')}
           </h1>
-          <Image
-            src="./assets/icons/title-line_FAQ.svg"
-            alt="title-line_FAQ"
-            width={136}
-            height={25}
-            style={{ strokeWidth: 0 }}
-            className="absolute bottom-0 left-1/2 transform -translate-x-1/2 z-0"
-          />
+          <svg
+            className="w-[66px] sm:w-[86px] lg:w-[156px] absolute bottom-0 left-1/2 transform -translate-x-1/2 z-0"
+            height="25"
+            viewBox="0 0 156 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10 10C10 10 52.4798 9.99988 69.6009 13.9062C86.7221 17.8125 146 10 146 10"
+              stroke="#C3E6FF"
+              strokeWidth="20"
+              strokeLinecap="round"
+            />
+          </svg>
         </div>
 
         <div className="flex flex-col">
